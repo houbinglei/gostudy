d8c6a46a36b8237d34ab4d8c29ff37f171fedca5
binglei.hou <393353713@qq.com>
2021-01-27 14:15:32 +0800

    day1

--- day1/main.go
+++ day1/main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+//  在一个const声明里，第一次出现iota，是0 ， 新增一行变量声明，iota 加1
+const (
+	n1 = iota
+	n2
+	_
+	n3
+)
+
+// 定义数量级
+const (
+	_  = iota
+	KB = 1 << (10 * iota) // 1 变成 10000000000（二进制） 转换成10进制 是1024
+	MB = 1 << (10 * iota)
+)
+
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(n1, n2, n3)
 }

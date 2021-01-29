738ca3d0fc7be4f07c15d0fa0484df69348933e0
binglei.hou <393353713@qq.com>
2021-01-29 11:34:25 +0800

    chengfabiao

---
+++ day1/字符.go
@@ -0,0 +1,17 @@
+//字符用单引号（’）包裹起来
+
+package main
+
+// func main() {
+// 	s := "hello沙河"
+// 	// fmt.Println(len(s))           // 11
+// 	// for i := 0; i < len(s); i++ { // , byte
+// 	// 	// fmt.Printf("%v(%c)", s[i], s[i])
+// 	// 	fmt.Printf("%c\n", s[i])	// %c 是英文字符，
+// 	// }
+
+// 	for _, r := range s { //rune
+// 		fmt.Printf("%v(%c) ", r, r)
+// 	}
+// 	fmt.Println()
+// }
